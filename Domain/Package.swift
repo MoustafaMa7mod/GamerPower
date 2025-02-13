9533d8cdd9f62abfd21b92a22f0525d302d3ce4b
MoustafaMa7mod <mostafa.akrap@gmail.com>
2025-02-13 14:57:41 +0200

    add data and domain local packages

---
+++ Domain/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Domain",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "Domain",
+            targets: ["Domain"]),
+    ],
+    dependencies: [
+        .package(path: "../NetworkLayer")
+    ],
+    targets: [
+        .target(
+            name: "Domain",
+            dependencies: [
+                "NetworkLayer"
+            ]
+        ),
+        .testTarget(
+            name: "DomainTests",
+            dependencies: ["Domain"]
+        ),
+    ]
+)
--- NetworkLayer/Sources/NetworkLayer/NetworkLayer.swift
+++ NetworkLayer/Sources/NetworkLayer/NetworkLayer.swift
@@ -1,2 +1,49 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Moya
+
+enum API {
+    case popular
+    case movie(movieId: String)
+    case search(query: String)
+}
+
+extension API: TargetType {
+    var baseURL: URL {
+        guard let url = URL(string: "https://api.themoviedb.org/3/") else { fatalError() }
+        return url
+    }
+    
+    var path: String {
+        switch self {
+        case .popular:
+            return "movie/popular"
+        case .movie(let movieId):
+            return "movie/\(movieId)"
+        case .search:
+            return "search/movie"
+        }
+    }
+    
+    var method: Method {
+        return .get
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var task: Task {
+        switch self {
+        case .popular, .movie:
+            return .requestParameters(parameters: ["api_key": Constants.API.apiKey], encoding: URLEncoding.queryString)
+        case .search(let query):
+            return .requestParameters(parameters: ["query" : query, "api_key": Constants.API.apiKey], encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return nil
+    }
+}
