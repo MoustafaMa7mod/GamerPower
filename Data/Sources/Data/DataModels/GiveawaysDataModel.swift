3c4848562baa9415062e685ad05aba365b171c15
MoustafaMa7mod <mostafa.akrap@gmail.com>
2025-02-13 17:03:55 +0200

    add data in data models and entities

--- Data/Sources/Data/DataModels/GiveawaysDataModel.swift
+++ Data/Sources/Data/DataModels/GiveawaysDataModel.swift
@@ -10,8 +10,33 @@ import Domain
 public struct GiveawaysDataModel: Codable {
     
     let id: Int?
+    let title: String?
+    let worth: String?
+    let image: String?
+    let description: String?
+    let instructions: String?
+    let endDate: String?
+    let type: String?
+    let platforms: String?
+    let users: Int?
     
+    enum CodingKeys: String, CodingKey {
+        case endDate = "end_date"
+        case id, title, worth, image, description, instructions, type, platforms, users
+    }
+
     public func toDomain() -> GiveawayItems {
-        GiveawayItems(id: id ?? 0)
+        GiveawayItems(
+            id: id ?? 0,
+            title: title ?? "",
+            worth: worth ?? "",
+            image: image ?? "",
+            description: description ?? "",
+            instructions: instructions ?? "",
+            expiryDate: endDate ?? "",
+            gameType: type ?? "",
+            platforms: platforms ?? "",
+            numberOfUsers: users ?? 0
+        )
     }
 }
--- Domain/Sources/Domain/Entities/GiveawayItems.swift
+++ Domain/Sources/Domain/Entities/GiveawayItems.swift
@@ -8,10 +8,37 @@
 public struct GiveawayItems {
     
     public let id: Int
+    public let title: String
+    public let worth: String
+    public let image: String
+    public let description: String
+    public let instructions: String
+    public let expiryDate: String
+    public let gameType: String
+    public let platforms: String
+    public let numberOfUsers: Int
 
     public init(
-        id: Int
+        id: Int,
+        title: String,
+        worth: String,
+        image: String,
+        description: String,
+        instructions: String,
+        expiryDate: String,
+        gameType: String,
+        platforms: String,
+        numberOfUsers: Int
     ) {
         self.id = id
+        self.title = title
+        self.worth = worth
+        self.image = image
+        self.description = description
+        self.instructions = instructions
+        self.expiryDate = expiryDate
+        self.gameType = gameType
+        self.platforms = platforms
+        self.numberOfUsers = numberOfUsers
     }
 }
