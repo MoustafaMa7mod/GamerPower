58272ffa83474daa510bf1212cb12ac05587a400
MoustafaMa7mod <mostafa.akrap@gmail.com>
2025-02-14 16:16:46 +0200

    add app delegate in project to handle navigation using coordinator

---
+++ GamerPower/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  GamerPower
+//
+//  Created by Mostafa Mahmoud on 14/02/2025.
+//
+
+import UIKit
+import SwiftUI
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        // Set Root View Controller with SwiftUI
+        let contentView = ContentView()
+        window?.rootViewController = UIHostingController(rootView: contentView)
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
+
